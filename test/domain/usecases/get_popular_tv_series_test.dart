ee266661051d749ffb8596a591bc3627bcbcb527
yogadarma13 <kadekyoga125@gmail.com>
2022-01-07 13:01:02 +0700

    test: should get list of popular tv series from the repository (popular tv series - use case)

---
+++ test/domain/usecases/get_popular_tv_series_test.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/get_airing_today_tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetPopularTvSeries useCase;
+  late MockMovieRepository mockMovieRepository;
+
+  setUp(() {
+    mockMovieRepository = MockMovieRepository();
+    useCase = GetAiringTodayTvSeries(mockMovieRepository);
+  });
+
+  final popularTvSeries = <Movie>[];
+
+  test('should get list of popular tv series from the repository', () async {
+    when(mockMovieRepository.getPopularTVSeries())
+        .thenAnswer((_) async => Right(popularTvSeries));
+
+    final resultList = await useCase.execute();
+    expect(resultList, Right(popularTvSeries));
+  });
+}
