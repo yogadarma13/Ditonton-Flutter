0251651c065368800ad130152a522d7a2dd410bd
yogadarma13 <kadekyoga125@gmail.com>
2022-01-19 00:37:46 +0700

    test: Page should display progress bar when loading (watchlist movies page)

---
+++ test/presentation/pages/watchlist_movies_page_test.dart
@@ -0,0 +1,41 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/presentation/pages/watchlist_movies_page.dart';
+import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+
+import 'watchlist_movies_page_test.mocks.dart';
+
+@GenerateMocks([WatchlistMovieNotifier])
+void main() {
+  late MockWatchlistMovieNotifier mockNotifier;
+
+  setUp(() {
+    mockNotifier = MockWatchlistMovieNotifier();
+  });
+
+  Widget _makeTestableWidget(Widget body) {
+    return ChangeNotifierProvider<WatchlistMovieNotifier>.value(
+      value: mockNotifier,
+      child: MaterialApp(
+        home: body,
+      ),
+    );
+  }
+
+  testWidgets('Page should display progress bar when loading',
+      (WidgetTester tester) async {
+    when(mockNotifier.watchlistState).thenReturn(RequestState.Loading);
+
+    final progressFinder = find.byType(CircularProgressIndicator);
+    final centerFinder = find.byType(Center);
+
+    await tester.pumpWidget(_makeTestableWidget(WatchlistMoviesPage()));
+
+    expect(centerFinder, findsOneWidget);
+    expect(progressFinder, findsOneWidget);
+  });
+}
