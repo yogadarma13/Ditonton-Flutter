3c0a2a45fbb2681d84947b34e5ef3373fc09419e
yogadarma13 <kadekyoga125@gmail.com>
2022-01-19 00:45:46 +0700

    test: Page should display text with message when Error (watchlist movies page)

--- test/presentation/pages/watchlist_movies_page_test.dart
+++ test/presentation/pages/watchlist_movies_page_test.dart
@@ -51,4 +51,16 @@ void main() {
 
     expect(listViewFinder, findsOneWidget);
   });
+
+  testWidgets('Page should display text with message when Error',
+      (WidgetTester tester) async {
+    when(mockNotifier.watchlistState).thenReturn(RequestState.Error);
+    when(mockNotifier.message).thenReturn('Error message');
+
+    final textFinder = find.byKey(Key('error_message'));
+
+    await tester.pumpWidget(_makeTestableWidget(WatchlistMoviesPage()));
+
+    expect(textFinder, findsOneWidget);
+  });
 }
