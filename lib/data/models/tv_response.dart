f283202a6c48e17a9c187671dbd2dd6202274d28
yogadarma13 <kadekyoga125@gmail.com>
2022-01-31 00:22:16 +0700

    add filter on Tv Response

--- lib/data/models/tv_response.dart
+++ lib/data/models/tv_response.dart
@@ -7,8 +7,10 @@ class TvResponse extends Equatable {
   TvResponse({required this.tvList});
 
   factory TvResponse.fromJson(Map<String, dynamic> json) => TvResponse(
-      tvList:
-          List<TvModel>.from(json["results"].map((x) => TvModel.fromJson(x))));
+        tvList: List<TvModel>.from((json["results"] as List)
+            .map((x) => TvModel.fromJson(x))
+            .where((element) => element.posterPath != null)),
+      );
 
   Map<String, dynamic> toJson() => {
         "results": List<dynamic>.from(tvList.map((x) => x.toJson())),
