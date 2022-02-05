f3ecc67e826d97ca1b75747033a92718adec1247
yogadarma13 <kadekyoga125@gmail.com>
2022-02-06 00:45:50 +0700

    remove variable window on AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,8 +4,6 @@ import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  var window: UIWindow?
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
