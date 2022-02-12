7e8b078e352d12d830d39e8a03a5df6999a07ad7
yogadarma13 <kadekyoga125@gmail.com>
2022-02-12 17:33:37 +0700

    create directory for test

--- test/presentation/bloc/watchlist/watchlist_tv_series_bloc_test.dart
+++ test/presentation/bloc/watchlist/watchlist_tv_series_bloc_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../dummy_data/dummy_objects.dart';
+import '../../../dummy_data/dummy_objects.dart';
 import 'watchlist_tv_series_bloc_test.mocks.dart';
 
 @GenerateMocks([GetWatchlistMoviesUseCase])
