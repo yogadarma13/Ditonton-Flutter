ea2de57ee97d14fa4b09630682c2b37c6a2324e9
yogadarma13 <kadekyoga125@gmail.com>
2022-02-22 00:30:36 +0700

    make integration test to open tv series page and search tv series

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -52,6 +52,48 @@ void main() {
     expect(find.byType(HomePage), findsOneWidget);
   });
 
+  testWidgets('open TV Series page and search tv series', (tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    final menuIcon = find.byKey(Key("menu_button"));
+    await tester.tap(menuIcon);
+    await tester.pumpAndSettle();
+
+    final movieButton = find.text('TV Series');
+    expect(movieButton, findsOneWidget);
+    await tester.tap(movieButton);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(HomeMoviePage), findsWidgets);
+    expect(find.text('Now Playing'), findsOneWidget);
+    expect(find.text('Popular'), findsOneWidget);
+    expect(find.text('Top Rated'), findsOneWidget);
+    expect(find.byType(ListView), findsWidgets);
+
+    final searchButton = find.byKey(Key('search'));
+    expect(searchButton, findsOneWidget);
+    await tester.tap(searchButton);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(SearchPage), findsOneWidget);
+    expect(find.text('Search TV Series'), findsOneWidget);
+    expect(find.byKey(Key('empty_image')), findsOneWidget);
+    expect(find.text('No Data'), findsOneWidget);
+
+    final searchTextField = find.byType(TextField);
+    expect(searchTextField, findsOneWidget);
+    await tester.enterText(searchTextField, 'victoria');
+    await tester.pumpAndSettle(Duration(seconds: 2));
+    expect(find.byType(ListView), findsOneWidget);
+
+    await tester.tap(find.byTooltip('Back'));
+    await tester.pumpAndSettle();
+    await tester.tap(find.byTooltip('Back'));
+    await tester.pumpAndSettle();
+    expect(find.byType(HomePage), findsOneWidget);
+  });
+
   testWidgets('open about page', (tester) async {
     app.main();
     await tester.pumpAndSettle();
